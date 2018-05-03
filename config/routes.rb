236d0bef29eb1180b1911bcf373254fe784fdee2
Talha Junaid <talha.junaid@amblersaleem.com>
2018-05-03 17:26:19 +0500

    Add contact page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-  get "about", to: "pages#about"
+  get "about",      to: "pages#about"
+  get "contact-us", to: "pages#contact"
 
   root to: "pages#index"
 
