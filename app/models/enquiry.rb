a7a0c6dca41b46b71bd6517fbaad9f45d4de2814
Talha Junaid <talha.junaid@amblersaleem.com>
2018-05-09 10:24:12 +0500

    Add enum and validations.

--- app/models/enquiry.rb
+++ app/models/enquiry.rb
@@ -1,3 +1,17 @@
 class Enquiry < ApplicationRecord
-  validates :email, :project_title, :project_description, presence: true
+  enum project_type: {
+    web_app:    1,
+    mobile_app: 2
+  }
+
+  enum service_required: {
+    engineering:        1,
+    ui_design:          2,
+    quality_assurance:  3,
+    ux_design:          4,
+    project_management: 5
+  }
+
+  validates :email, :project_title, :project_description, :project_type, :service_required, :phone_number,
+            presence: true
 end
